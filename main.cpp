8ffbc9a98796f5c6b805df78a3395af117905ccc
Boris Ulyanov <boris-ulyanov@mail.ru>
2020-01-09 01:56:42 +0300

    out_to_file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 
+#include "out_to_file.h"
 #include "out_to_term.h"
 #include "string_processor.h"
 
@@ -15,6 +16,7 @@ int main(int argc, const char* argv[]) {
     StringProcessor processor(default_pack_size);
 
     [[maybe_unused]] auto out_to_term = OutToTerm();
+    [[maybe_unused]] auto out_to_file = OutToFile();
 
     for (std::string line; std::getline(std::cin, line);) {
         processor.add(line);
---
+++ toolbox/out_to_file.h
@@ -0,0 +1,44 @@
+/**
+ * @brief Output packed commands to file
+ */
+
+#pragma once
+
+#include "emitter.h"
+#include "storage.h"
+
+#include <ctime>
+#include <fstream>
+
+class OutToFile : Observer {
+    std::time_t first_cmd__time;
+
+    void on_first_command() {
+        first_cmd__time = std::time(nullptr);
+    }
+
+    void on_end_pack() {
+        const auto filename = std::to_string(first_cmd__time) + std::string(".log");
+        std::ofstream file;
+        file.open(filename);
+
+        const auto& stor = Storage::Instance();
+        const auto size = stor.size();
+        for (std::size_t i = 0; i < size; ++i) file << stor[i] << std::endl;
+        file.close();
+    }
+
+   public:
+    OutToFile() {
+        first_cmd__time = -1;
+        Emitter::Instance().subscribe(Event::FIRST_COMMAND, this);
+        Emitter::Instance().subscribe(Event::END_PACK, this);
+    }
+
+    void on_event([[maybe_unused]] Event e) {
+        if (e == Event::FIRST_COMMAND)
+            on_first_command();
+        else
+            on_end_pack();
+    }
+};
